430cbfa8ff48e66d172bbee22d0eee29d3e37a91
mentalhealthawhereness <38903439+mentalhealthawhereness@users.noreply.github.com>
2020-01-14 16:04:17 +0800

    Update insert_function.sql

--- sql/insert_function.sql
+++ sql/insert_function.sql
@@ -3,7 +3,7 @@ DROP FUNCTION IF EXISTS insert_crowd_mapping_data(text,text,text,text,text,text,
 
 CREATE OR REPLACE FUNCTION insert_crowd_mapping_data (
     _geojson TEXT,
-    _fname TEXT,
+    _name TEXT,
     _alt TEXT,
     _region TEXT,
     _muni TEXT,
@@ -23,7 +23,7 @@ BEGIN
 	
 
 	--Executes the insert given the supplied geometry, description, and username, while protecting against SQL injection.
-    EXECUTE ' INSERT INTO '||quote_ident(_the_table)||' (the_geom, fname, alt, region, muni, type, source, researcher)
+    EXECUTE ' INSERT INTO '||quote_ident(_the_table)||' (the_geom, name, alt, region, muni, type, source, researcher)
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
             ' USING _the_geom, _fname, _alt, _region, _muni, _type, _source, _researcher;
             
