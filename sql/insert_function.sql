8a74f84fa59acc8f8d8f7ae1884593a8043ab11e
mentalhealthawhereness <38903439+mentalhealthawhereness@users.noreply.github.com>
2020-01-14 15:47:46 +0800

    Update insert_function.sql

--- sql/insert_function.sql
+++ sql/insert_function.sql
@@ -3,31 +3,29 @@ DROP FUNCTION IF EXISTS insert_crowd_mapping_data(text,text,text,text,text,text,
 
 CREATE OR REPLACE FUNCTION insert_crowd_mapping_data (
     _geojson TEXT,
-    _address TEXT,
-    _name TEXT,
-    _doctor TEXT,
-    _contactnum TEXT,
-    _email TEXT,	
-    _fee TEXT, 
-    _website TEXT,
-    _hours TEXT,
-    _notes TEXT)    
+    _fname TEXT,
+    _alt TEXT,
+    _region TEXT,
+    _muni TEXT,
+    _type TEXT,	
+    _source TEXT, 
+    _researcher TEXT)    
 --Has to return something in order to be used in a "SELECT" statement
 RETURNS integer
 AS $$
 DECLARE 
     _the_geom GEOMETRY;
 	--The name of your table in cartoDB
-	_the_table TEXT := 'mhloc';
+	_the_table TEXT := 'taal';
 BEGIN
     --Convert the GeoJSON to a geometry type for insertion. 
     _the_geom := ST_SetSRID(ST_GeomFromGeoJSON(_geojson),4326); 
 	
 
 	--Executes the insert given the supplied geometry, description, and username, while protecting against SQL injection.
-    EXECUTE ' INSERT INTO '||quote_ident(_the_table)||' (the_geom, address, name, doctor, contactnum, email, fee, website, hours, notes)
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
-            ' USING _the_geom, _address, _name, _doctor, _contactnum, _email, _fee, _website, _hours, _notes;
+    EXECUTE ' INSERT INTO '||quote_ident(_the_table)||' (the_geom, fname, alt, region, muni, type, source, researcher)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+            ' USING _the_geom, _fname, _alt, _region, _muni, _type, _source, _researcher;
             
     RETURN 1;
 END;
@@ -35,4 +33,4 @@ $$
 LANGUAGE plpgsql SECURITY DEFINER ;
 
 --Grant access to the public user
-GRANT EXECUTE ON FUNCTION insert_crowd_mapping_data( text, text, text, text, text, text, text, text, text, text) TO publicuser;
+GRANT EXECUTE ON FUNCTION insert_crowd_mapping_data( text, text, text, text, text, text, text, text) TO publicuser;
